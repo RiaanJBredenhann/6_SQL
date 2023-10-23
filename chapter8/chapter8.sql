f65360eb135af4ec05a3a8ea9735c3c64fa719b7
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-10-23 09:40:21 +0200

    uodated chapter 8 with source code

--- chapter8/chapter8.sql
+++ chapter8/chapter8.sql
@@ -1,4 +1,4 @@
--- Listing 8-1 --
+-- Listing 8-1: Creating and filling the 2014 Public Libraries Survey table
 
 CREATE TABLE pls_fy2014_pupld14a (
     stabr varchar(2) NOT NULL,
@@ -81,10 +81,10 @@ CREATE INDEX city2014_idx ON pls_fy2014_pupld14a (city);
 CREATE INDEX visits2014_idx ON pls_fy2014_pupld14a (visits);
 
 COPY pls_fy2014_pupld14a
-FROM 'C:\YourDirectory\chapter8\pls_fy2014_pupld14a.csv'
+FROM 'C:\YourDirectory\/chapter8\pls_fy2014_pupld14a.csv'
 WITH (FORMAT CSV, HEADER);
 
--- Listing 8-2 --
+-- Listing 8-2: Creating and filling the 2009 Public Libraries Survey table
 
 CREATE TABLE pls_fy2009_pupld09a (
     stabr varchar(2) NOT NULL,
@@ -166,7 +166,7 @@ COPY pls_fy2009_pupld09a
 FROM 'C:\YourDirectory\chapter8\pls_fy2009_pupld09a.csv'
 WITH (FORMAT CSV, HEADER);
 
--- Listing 8-3 --
+-- Listing 8-3: Using count() for table row counts
 
 SELECT count(*)
 FROM pls_fy2014_pupld14a;
@@ -174,12 +174,12 @@ FROM pls_fy2014_pupld14a;
 SELECT count(*)
 FROM pls_fy2009_pupld09a;
 
--- Listing 8-4 --
+-- Listing 8-4: Using count() for the number of values in a column
 
 SELECT count(salaries)
 FROM pls_fy2014_pupld14a;
 
--- Listing 8-5 --
+-- Listing 8-5: Using count() for the number of distinct values in a column
 
 SELECT count(libname)
 FROM pls_fy2014_pupld14a;
@@ -187,55 +187,76 @@ FROM pls_fy2014_pupld14a;
 SELECT count(DISTINCT libname)
 FROM pls_fy2014_pupld14a;
 
--- listing 8-6 --
+-- Bonus: find duplicate libnames
+SELECT libname, count(libname)
+FROM pls_fy2014_pupld14a
+GROUP BY libname
+ORDER BY count(libname) DESC;
 
+-- Bonus: see location of every Oxford Public Library
+SELECT libname, city, stabr
+FROM pls_fy2014_pupld14a
+WHERE libname = 'OXFORD PUBLIC LIBRARY';
+
+-- Listing 8-6: Finding the most and fewest visits using max() and min()
 SELECT max(visits), min(visits)
 FROM pls_fy2014_pupld14a;
 
--- Listing 8-7 --
+-- Listing 8-7: Using GROUP BY on the stabr column
 
+-- There are 56 in 2014.
 SELECT stabr
 FROM pls_fy2014_pupld14a
 GROUP BY stabr
 ORDER BY stabr;
 
+-- Bonus: there are 55 in 2009.
 SELECT stabr
 FROM pls_fy2009_pupld09a
 GROUP BY stabr
 ORDER BY stabr;
 
--- Listing 8-8 --
+-- Listing 8-8: Using GROUP BY on the city and stabr columns
 
 SELECT city, stabr
 FROM pls_fy2014_pupld14a
 GROUP BY city, stabr
 ORDER BY city, stabr;
 
--- Listing 8-9 --
+-- Bonus: We can count some of the combos
+SELECT city, stabr, count(*)
+FROM pls_fy2014_pupld14a
+GROUP BY city, stabr
+ORDER BY count(*) DESC;
+
+-- Listing 8-9: GROUP BY with count() on the stabr column
 
 SELECT stabr, count(*)
 FROM pls_fy2014_pupld14a
 GROUP BY stabr
 ORDER BY count(*) DESC;
 
--- Listing 8-10 --
+-- Listing 8-10: GROUP BY with count() on the stabr and stataddr columns
 
 SELECT stabr, stataddr, count(*)
 FROM pls_fy2014_pupld14a
 GROUP BY stabr, stataddr
 ORDER BY stabr ASC, count(*) DESC;
 
--- Listing 8-11 --
+-- Listing 8-11: Using the sum() aggregate function to total visits to
+-- libraries in 2014 and 2009
 
+-- 2014
 SELECT sum(visits) AS visits_2014
 FROM pls_fy2014_pupld14a
 WHERE visits >= 0;
 
+-- 2009
 SELECT sum(visits) AS visits_2009
 FROM pls_fy2009_pupld09a
 WHERE visits >= 0;
 
--- Listing 8-12 --
+-- Listing 8-12: Using sum() to total visits on joined 2014 and 2009 library tables
 
 SELECT sum(pls14.visits) AS visits_2014,
        sum(pls09.visits) AS visits_2009
@@ -243,7 +264,7 @@ FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
 ON pls14.fscskey = pls09.fscskey
 WHERE pls14.visits >= 0 AND pls09.visits >= 0;
 
--- Listing 8-13 --
+-- Listing 8-13: Using GROUP BY to track percent change in library visits by state
 
 SELECT pls14.stabr,
        sum(pls14.visits) AS visits_2014,
@@ -256,7 +277,7 @@ WHERE pls14.visits >= 0 AND pls09.visits >= 0
 GROUP BY pls14.stabr
 ORDER BY pct_change DESC;
 
--- Listing 8-14 --
+-- Listing 8-14: Using HAVING to filter the results of an aggregate query
 
 SELECT pls14.stabr,
        sum(pls14.visits) AS visits_2014,
@@ -269,25 +290,3 @@ WHERE pls14.visits >= 0 AND pls09.visits >= 0
 GROUP BY pls14.stabr
 HAVING sum(pls14.visits) > 50000000
 ORDER BY pct_change DESC;
-
--- Try It Yourself --
--- Q1 --
-
--- Q2 --
-
--- Q3 --
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
