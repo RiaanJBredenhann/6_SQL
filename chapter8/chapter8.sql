1f2c523142551bdcd59852e1e58baaca217af80b
RiaanJBredenhann <riaanbredenhann@gmail.com>
2023-10-23 09:43:43 +0200

    updated wording on 8-14 for readability

--- chapter8/chapter8.sql
+++ chapter8/chapter8.sql
@@ -284,7 +284,7 @@ SELECT pls14.stabr,
        sum(pls09.visits) AS visits_2009,
        round( (CAST(sum(pls14.visits) AS decimal(10,1)) - sum(pls09.visits)) /
                     sum(pls09.visits) * 100, 2 ) AS pct_change
-FROM pls_fy2014_pupld14a pls14 JOIN pls_fy2009_pupld09a pls09
+FROM pls_fy2014_pupld14a AS pls14 JOIN pls_fy2009_pupld09a AS pls09
 ON pls14.fscskey = pls09.fscskey
 WHERE pls14.visits >= 0 AND pls09.visits >= 0
 GROUP BY pls14.stabr
